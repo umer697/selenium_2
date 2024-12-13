a168dccdc1e70c868c54151b4687805e7a3fcad0
Umer Farooq <umerfarooq.697@gmail.com>
2024-12-13 10:59:46 +0500

    comment updated in fk file

--- fk.py
+++ fk.py
@@ -11,7 +11,7 @@ chrome_options.add_experimental_option("detach",True)
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=chrome_options)
 driver.maximize_window()
-# Open a website
+# Open a website and start get data from the website
 driver.get("https://www.flipkart.com/samsung-galaxy-fit3-amoled-display-aluminium-body-upto-13day-battery-5atm-ip68/p/itmc884180dbd9a8?pid=SMWGY6YRNHBBK8FP&lid=LSTSMWGY6YRNHBBK8FPDPQEGB&marketplace=FLIPKART&srno=s_1_33&otracker=search&otracker1=search&fm=Search&iid=226e103e-2f95-480f-aa88-43f968ed1637.SMWGY6YRYKY3GXQU.SEARCH&ppt=sp&ppn=sp&ssid=2mm4kdaxzk0000001734019004923&qH=87968ec020b2016b")
 
 watch_name =  driver.find_element(By.XPATH,'/html/body/div[1]/div/div[3]/div[1]/div[2]/div[2]/div/div[1]/h1/span').text
